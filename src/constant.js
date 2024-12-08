d2f5d234a31836f83eb0574afe122fb469708fc7
Deepa Das <114664517+THECODEZON@users.noreply.github.com>
2024-12-09 01:59:34 +0530

    Made UI of toolbox

---
+++ src/constant.js
@@ -0,0 +1,17 @@
+const COLORS={
+    BLACK:'black',
+    WHITE:'white',
+    RED:'red',
+    GREEN:'green',
+    BLUE:'blue',
+    YELLOW:'yellow',
+    PURPLE:'purple',
+    ORANGE:'orange',
+    PINK:'pink',
+    CYAN:'cyan',
+    GRAY:'gray',
+    BROWN:'brown',
+
+}
+
+export {COLORS};
